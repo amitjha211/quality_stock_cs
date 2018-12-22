e3339c8a2351a0eebdfe8ac10d51779b6eb8752f
tyronmaxico2008 <amitjha211@outlook.com>
2018-12-22 19:42:30 +0530

    first commit

---
+++ bhavDataAnalysis/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace bhavDataAnalysis
+{
+    partial class frmTest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblFilePath = new System.Windows.Forms.Label();
+            this.btnOpenCSV = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.chkFilter = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtQuickSearch = new System.Windows.Forms.TextBox();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.txtDeliveryPer = new System.Windows.Forms.TextBox();
+            this.txtVolume = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblFilePath);
+            this.panel1.Controls.Add(this.btnOpenCSV);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(853, 33);
+            this.panel1.TabIndex = 0;
+            // 
+            // lblFilePath
+            // 
+            this.lblFilePath.AutoSize = true;
+            this.lblFilePath.Location = new System.Drawing.Point(85, 11);
+            this.lblFilePath.Name = "lblFilePath";
+            this.lblFilePath.Size = new System.Drawing.Size(45, 13);
+            this.lblFilePath.TabIndex = 1;
+            this.lblFilePath.Text = "FilePath";
+            // 
+            // btnOpenCSV
+            // 
+            this.btnOpenCSV.Location = new System.Drawing.Point(8, 7);
+            this.btnOpenCSV.Name = "btnOpenCSV";
+            this.btnOpenCSV.Size = new System.Drawing.Size(67, 22);
+            this.btnOpenCSV.TabIndex = 0;
+            this.btnOpenCSV.Text = "Load CSV";
+            this.btnOpenCSV.UseVisualStyleBackColor = true;
+            this.btnOpenCSV.Click += new System.EventHandler(this.btnOpenCSV_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.groupBox1);
+            this.panel2.Controls.Add(this.checkedListBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 33);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 263);
+            this.panel2.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.chkFilter);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtQuickSearch);
+            this.groupBox1.Controls.Add(this.btnShow);
+            this.groupBox1.Controls.Add(this.txtDeliveryPer);
+            this.groupBox1.Controls.Add(this.txtVolume);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(8, 90);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(185, 165);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Filter information";
+            // 
+            // chkFilter
+            // 
+            this.chkFilter.AutoSize = true;
+            this.chkFilter.Location = new System.Drawing.Point(13, 137);
+            this.chkFilter.Name = "chkFilter";
+            this.chkFilter.Size = new System.Drawing.Size(80, 17);
+            this.chkFilter.TabIndex = 6;
+            this.chkFilter.Text = "Apply Filter";
+            this.chkFilter.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Symbol Search";
+            // 
+            // txtQuickSearch
+            // 
+            this.txtQuickSearch.Location = new System.Drawing.Point(10, 109);
+            this.txtQuickSearch.Name = "txtQuickSearch";
+            this.txtQuickSearch.Size = new System.Drawing.Size(169, 21);
+            this.txtQuickSearch.TabIndex = 2;
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(119, 135);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(60, 23);
+            this.btnShow.TabIndex = 4;
+            this.btnShow.Text = "Show";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // txtDeliveryPer
+            // 
+            this.txtDeliveryPer.Location = new System.Drawing.Point(10, 72);
+            this.txtDeliveryPer.Name = "txtDeliveryPer";
+            this.txtDeliveryPer.Size = new System.Drawing.Size(170, 21);
+            this.txtDeliveryPer.TabIndex = 3;
+            this.txtDeliveryPer.Text = "45";
+            // 
+            // txtVolume
+            // 
+            this.txtVolume.Location = new System.Drawing.Point(10, 33);
+            this.txtVolume.Name = "txtVolume";
+            this.txtVolume.Size = new System.Drawing.Size(170, 21);
+            this.txtVolume.TabIndex = 2;
+            this.txtVolume.Text = "50000";
+            this.txtVolume.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Delivery Per (%) Above";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Volume Above";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(8, 8);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(185, 68);
+            this.checkedListBox1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(206, 45);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(639, 246);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // frmTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(853, 296);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "frmTest";
+            this.Text = "Bhav Data Analysis";
+            this.Load += new System.EventHandler(this.frmTest_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnOpenCSV;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label lblFilePath;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtDeliveryPer;
+        private System.Windows.Forms.TextBox txtVolume;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.TextBox txtQuickSearch;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox chkFilter;
+        private System.Windows.Forms.BindingSource bindingSource1;
+    }
+}
+
---
+++ bhavDataAnalysis/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace bhavDataAnalysis
+{
+    public partial class frmTest : Form
+    {
+        public frmTest()
+        {
+            InitializeComponent();
+        }
+
+        DataTable t = null;
+
+        private void btnOpenCSV_Click(object sender, EventArgs e)
+        {
+            var oFile = new System.Windows.Forms.OpenFileDialog();
+
+            oFile.Filter = "CSV File|*.csv";
+
+            if (oFile.ShowDialog() == DialogResult.OK)
+            {
+                lblFilePath.Text = oFile.FileName;
+                t = g.readCSV(lblFilePath.Text);
+                //dataGridView1.DataSource = getTable();
+
+                t = getTable();
+
+                showColumnsInformation(t);
+
+            }
+        }
+
+        private DataTable getTable()
+        {
+            DataTable tTmp = g.readCSV(lblFilePath.Text);
+            foreach (DataColumn col in tTmp.Columns) col.ColumnName = col.ColumnName.Trim();
+
+
+            t = new DataTable();
+            t.TableName = "bhavData";
+
+
+            t.Columns.Add("SYMBOL", typeof(string));
+            t.Columns.Add("SERIES", typeof(string));
+            t.Columns.Add("DATE1", typeof(string));
+            t.Columns.Add("PREV_CLOSE", typeof(decimal));
+            t.Columns.Add("OPEN_PRICE", typeof(decimal));
+            t.Columns.Add("HIGH_PRICE", typeof(decimal));
+            t.Columns.Add("LOW_PRICE", typeof(decimal));
+            t.Columns.Add("LAST_PRICE", typeof(decimal));
+            t.Columns.Add("CLOSE_PRICE", typeof(decimal));
+            t.Columns.Add("AVG_PRICE", typeof(decimal));
+            t.Columns.Add("TTL_TRD_QNTY", typeof(decimal));
+            t.Columns.Add("TURNOVER_LACS", typeof(decimal));
+            t.Columns.Add("NO_OF_TRADES", typeof(decimal));
+            t.Columns.Add("DELIV_QTY", typeof(decimal));
+            t.Columns.Add("DELIV_PER", typeof(decimal));
+
+            foreach (DataRow rSource in tTmp.Rows)
+            {
+                DataRow rDestination = t.NewRow();
+                rDestination["SYMBOL"] = rSource["SYMBOL"];
+                rDestination["SERIES"] = rSource["SERIES"];
+                rDestination["DATE1"] = rSource["DATE1"];
+
+                rDestination["PREV_CLOSE"] = g.parseDecimal(rSource["PREV_CLOSE"], 2);
+                rDestination["OPEN_PRICE"] = g.parseDecimal(rSource["OPEN_PRICE"], 2);
+                rDestination["HIGH_PRICE"] = g.parseDecimal(rSource["HIGH_PRICE"], 2);
+                rDestination["LOW_PRICE"] = g.parseDecimal(rSource["LOW_PRICE"], 2);
+                rDestination["LAST_PRICE"] = g.parseDecimal(rSource["LAST_PRICE"], 2);
+                rDestination["CLOSE_PRICE"] = g.parseDecimal(rSource["CLOSE_PRICE"], 2);
+                rDestination["AVG_PRICE"] = g.parseDecimal(rSource["AVG_PRICE"], 2);
+                rDestination["TTL_TRD_QNTY"] = g.parseDecimal(rSource["TTL_TRD_QNTY"], 2);
+                rDestination["TURNOVER_LACS"] = g.parseDecimal(rSource["TURNOVER_LACS"], 2);
+                rDestination["NO_OF_TRADES"] = g.parseDecimal(rSource["NO_OF_TRADES"], 2);
+                rDestination["DELIV_QTY"] = g.parseDecimal(rSource["DELIV_QTY"], 2);
+                rDestination["DELIV_PER"] = g.parseDecimal(rSource["DELIV_PER"], 2);
+
+                t.Rows.Add(rDestination);
+            }
+
+            return t;
+        }
+
+        private void showColumnsInformation(DataTable t)
+        {
+            foreach (DataColumn col in t.Columns)
+            {
+                checkedListBox1.Items.Add(col.ColumnName, true);
+            }
+        }
+
+        private void showData()
+        {
+
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmTest_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            bindingSource1.DataSource = t;
+            bindingSource1.Filter = " DELIV_PER > " + txtDeliveryPer.Text + " and  DELIV_QTY > " + txtVolume.Text;
+
+            dataGridView1.AutoGenerateColumns = false;
+            dataGridView1.Columns.Clear();
+
+            for (int i = 0; i < checkedListBox1.Items.Count; i++)
+            {
+                if (checkedListBox1.GetItemChecked(i) == true)
+                {
+                    string sField = checkedListBox1.GetItemText(checkedListBox1.Items[i]);
+                    dataGridView1.Columns.Add(sField, sField);
+                    dataGridView1.Columns[dataGridView1.Columns.Count - 1].DataPropertyName = sField;
+
+                }
+            }
+
+            dataGridView1.DataSource = bindingSource1;
+        }
+    }
+}
